0ed5e6d996a27dced465888f4ab2baecf884aba9
jian <jian@jiandeMacBook-Pro.local>
2021-05-20 18:00:56 +0800

    fix

--- structure.go
+++ structure.go
@@ -1,4 +1,4 @@
-package structure
+package main
 
 import "errors"
 
@@ -157,4 +157,50 @@ func (head *Node) FindNodeByIndex(index int) (node *Node, err error) {
 		}
 	}
 	return nil, nil
+}
+
+
+/**
+stack
+ */
+
+func GeneralStack(slice []int) *Node {
+
+	var head *Node
+
+	for _, val := range slice {
+		node := GeneralNode(val)
+		node.Next = head
+		head = node
+	}
+
+	return head
+}
+
+//push
+
+func PushStack(head *Node, val int) *Node {
+	node := GeneralNode(val)
+	node.Next = head
+	return  node
+}
+
+//pop
+func PopStack(heaad *Node) (newHead *Node, val int){
+
+	if heaad.IsEmpty() {
+		return heaad, -999
+	}
+
+	return heaad.Next, heaad.Val
+}
+
+//isEmpty
+
+func (head *Node)IsEmpty() bool {
+	if head == nil {
+		return  true
+	}else {
+		return  false
+	}
 }
--- structure_test.go
+++ structure_test.go
@@ -1,4 +1,4 @@
-package structure
+package main
 
 import (
 	"testing"
@@ -152,4 +152,51 @@ func TestFindNodeByIndex(t *testing.T) {
 	assert.Equal(t, nil, err)
 	assert.Equal(t, testSlice[5], node.Val)
 
+}
+
+
+
+/**
+stack
+*/
+
+
+var testStackSlice = []int{6, 5, 4, 3, 2, 1, 0}
+
+func TestGeneralStack(t *testing.T) {
+	head := GeneralStack(testStackSlice)
+	assert.Equal(t, testStackSlice[6], head.Val)
+	assert.Equal(t, testStackSlice[5], head.Next.Val)
+	assert.Equal(t, testStackSlice[4], head.Next.Next.Val)
+}
+
+func TestPushStack(t *testing.T) {
+	head := GeneralStack(testStackSlice)
+	insert := PushStack(head, -1)
+	assert.Equal(t, -1, insert.Val)
+	assert.Equal(t, 0, insert.Next.Val)
+	t.Log("push-stack ok")
+}
+
+func TestPopStack(t *testing.T) {
+	var head *Node
+	head, v := PopStack(head)
+	assert.Equal(t, -999, v)
+
+	head = GeneralStack(testStackSlice)
+	head, val := PopStack(head)
+	assert.Equal(t, testStackSlice[6], val)
+
+	head, va := PopStack(head)
+	assert.Equal(t, testStackSlice[5], va)
+}
+
+func TestIsEmpty(t *testing.T) {
+	var head *Node
+	isEmpty := head.IsEmpty()
+	assert.Equal(t, true, isEmpty)
+
+	head = GeneralStack(testStackSlice)
+	isEmpty = head.IsEmpty()
+	assert.Equal(t, false, isEmpty)
 }
